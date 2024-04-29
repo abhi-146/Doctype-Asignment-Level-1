a9bbdb35518213b55f7937523c68ce5f279b346e
Abhinav Jain <58175317+abhi-146@users.noreply.github.com>
2024-04-29 17:30:19 +0530

    Remove commented code

--- student_management/student_management/doctype/student/student.js
+++ student_management/student_management/doctype/student/student.js
@@ -7,50 +7,28 @@
 // 	},
 frappe.ui.form.on('Student', {
     refresh: function(frm) {
-        
+        // Add a custom button to the form
         frm.add_custom_button(__('Create User'), function() {
+            // When the button is clicked, call the server-side method to create the user
             frappe.call({
                 method: 'student_management.student_management.doctype.student.student.create_user_if_not_exists',
+                // Pass the name of the student document as an argument
                 args: {
                     name: frm.doc.name
                 },
+                // Handle the response from the server
                 callback: function(response) {
+                    // If the response contains a message
                     if (response.message) {
+                        // Display the message to the user
                         frappe.msgprint(response.message);
                     } else {
+                        // If there is no message in the response, display a generic failure message
                         frappe.msgprint('Failed to create user.');
                     }
                 }
             });
         });
     },
-
 });
 
-
-
-// function create_user(frm) {
-
-//     var email = frm.doc.email;
-//     var first_name = frm.doc.first_name;
-    
-//     if (!email) {
-//         frappe.msgprint('Please provide an email address.');
-//         return;
-//     }
-
-//     frappe.call({
-//         method: 'student_management.student_management.doctype.student.student.create_user_if_not_exists',
-//         args: {
-//             email: email,
-//             first_name: first_name
-//         },
-//         callback: function(r) {
-//             if (r.message === 'exists') {
-//                 frappe.msgprint('User already exists.');
-//             } else if (r.message === 'created') {
-//                 frappe.msgprint('User created successfully.');
-//             }
-//         }
-//     });
-// }
